2c1f8f983978f57c0c1a5fd2a62f1b9ecc72611a
br-rait <bhavesh@raitracker.com>
2025-12-30 17:14:01 +0530

    fix: test

--- core/settings.py
+++ core/settings.py
@@ -70,6 +70,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Your static files during development
+]
+
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
